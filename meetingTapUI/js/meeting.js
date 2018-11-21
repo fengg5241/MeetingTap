4e1e69695f2f86edf220edf6e9bbcf05aa92bd97
fengg5241 <fengg5241@users.noreply.github.com>
2018-11-21 17:37:03 +0800

    Update meeting.js

--- meetingTapUI/js/meeting.js
+++ meetingTapUI/js/meeting.js
@@ -3,17 +3,55 @@ $(document).ready(function() {
 
     editor = new $.fn.dataTable.Editor( {
         table: "#dataTables-example",
+        ajax: "https://40ddba2a-c79f-4843-9e76-02cad7cd8fea.mock.pstmn.io/meeting/getMeetings",
         fields: [ {
-                label: "Rendering engine:",
-                name: "Rendering engine"
-            }
+                label: "name",
+                name: "name"
+            },
+            {
+                label: "location",
+                name: "location"
+            },
+            {
+                label: "start date:",
+                name: "start_date",
+                type: "datetime"
+            },
+            {
+                label: "end date:",
+                name: "end_date",
+                type: "datetime"
+            },
         ]
+    } );
+
+    // Activate an inline edit on click of a table cell
+    $('#dataTables-example').on( 'click', 'tbody td:not(:first-child)', function (e) {
+        editor.inline( this );
     } );
 
     $('#dataTables-example').DataTable({
         responsive: true,
         dom: 'Bfrtip',
         select: true,
+        ajax: "https://40ddba2a-c79f-4843-9e76-02cad7cd8fea.mock.pstmn.io/meeting/getMeetings",
+        columns: [
+            {
+                data: null,
+                defaultContent: '',
+                className: 'select-checkbox',
+                orderable: false
+            },
+            { data: "name" },
+            { data: "location" },
+            { data: "start_date" },
+            { data: "end_date" }
+        ],
+        select: {
+            style:    'os',
+            selector: 'td:first-child'
+        },
+
         buttons: [
             { extend: 'create', editor: editor },
             { extend: 'edit',   editor: editor },
